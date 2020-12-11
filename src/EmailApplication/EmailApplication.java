ed000134c1af3d9a83b00e994f542f617bd223d1
deepmasiwal <deepmasiwal@3dipsemiconductors.onmicrosoft.com>
2020-12-12 02:31:27 +0530

    A sample email application

---
+++ src/EmailApplication/EmailApplication.java
@@ -0,0 +1,17 @@
+package EmailApplication;
+
+import java.util.Scanner;
+
+public class EmailApplication {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+//		Scanner sc=new Scanner(System.in); 
+//		Scanner sd=new Scanner(System.in);  
+		
+		Email em1 = new Email("Siddu", "M R");
+		
+		System.out.println(em1.showInfo());
+	}
+
+}
