21cc70fed70078805a99c44f66c3af4d060a5e46
deepmasiwal <deepmasiwal@3dipsemiconductors.onmicrosoft.com>
2020-12-12 19:58:19 +0530

    Email application updated

--- src/EmailApplication/EmailApplication.java
+++ src/EmailApplication/EmailApplication.java
@@ -6,10 +6,14 @@ public class EmailApplication {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-//		Scanner sc=new Scanner(System.in); 
-//		Scanner sd=new Scanner(System.in);  
-		
-		Email em1 = new Email("Siddu", "M R");
+		Scanner sc = new Scanner(System.in); 
+		System.out.println("Enter first name: ");
+		String firstName = sc.nextLine();
+		Scanner sd = new Scanner(System.in); 
+		System.out.println("Enter Last name: ");
+		String lastName = sd.nextLine();
+		System.out.println("Enter Last name: ");
+		Email em1 = new Email(firstName, lastName);
 		
 		System.out.println(em1.showInfo());
 	}
